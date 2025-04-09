7f7f755751aa2afae422ebc7665f5794dccfa531
Alexandre Bouchard <bouchard@stat.ubc.ca>
2025-04-08 21:12:09 -0700

    Basic working version

---
+++ README.md
@@ -0,0 +1,26 @@
+# Problem
+
+- on Compute Canada clusters (e.g., Beluga), Julia is basically unusable because of precompilation times
+- e.g. 2235 seconds for getting past `using Pigeons`!
+
+
+# Cause
+
+- after some investigations, it turns out the issue is the increasingly bloated number of files in Julia depot
+    - huge number of files, 100k not uncommon
+    - certain network file systems slow to a crawl when faced by that
+
+
+# Solution
+
+The script in `main.sh` implements a workaround. To use it:
+
+- Add to you `.bashrc`: 
+
+```
+source /path/to/main.sh
+```
+
+- When you make a change to your julia depot, type `save_depot`
+
+- If you have a second terminal open, use `load_depot` to force reloading it. 
---
+++ main.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+load_depot() {
+  TEMP_DIR=$(mktemp -d)
+  export JULIA_DEPOT_PATH="$TEMP_DIR/depot"
+  echo "Setting up depot into $JULIA_DEPOT_PATH"
+
+  if [ -f "$HOME/.depot.zip" ]; then
+    cp "$HOME/.depot.zip" "$TEMP_DIR/"
+    unzip -q "$TEMP_DIR/.depot.zip" -d "$TEMP_DIR/"
+  fi
+}
+export -f load_depot
+
+save_depot() {
+  if [ ! -d "$JULIA_DEPOT_PATH" ]; then
+    echo "Need to setup depot path first"
+    return
+  fi
+  
+  ZIP_FILE="$HOME/.depot.zip"
+  [ -f "$ZIP_FILE" ] && rm "$ZIP_FILE" 
+  PARENT_DIR="$(dirname "$JULIA_DEPOT_PATH")" 
+  cd $PARENT_DIR
+  zip -rq depot.zip depot
+  mv depot.zip "$ZIP_FILE"
+  cd - > /dev/null
+}
+export -f save_depot
+
+if [ -z "$JULIA_DEPOT_PATH" ]; then
+  setup_depot
+fi
